25619e8dd0cf792d9808a4e5dd478f2768675566
bluu <bluu@me.com>
2016-08-13 21:11:08 -0700

    Added sass support

--- app/app.jsx
+++ app/app.jsx
@@ -11,7 +11,7 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // App css
-require('style!css!application-styles');
+require('style!css!sass!application-styles');
 
 ReactDOM.render(
   <Router history={hashHistory}>
